4c8769ace5ceb2aea06efcc2d32c74b6289cb8e8
samuele.villa <samuele.villa.ufficiale@gmail.com>
2025-01-17 19:48:09 +0100

    [NVIM] Changed colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,21 @@
 return {
-	"ellisonleao/gruvbox.nvim",
-	lazy = false,
-	priority = 1000,
-	name = "gruvbox",
-	config = function()
-		vim.o.background = "dark" -- or "light" for light mode
-		vim.cmd([[colorscheme gruvbox]])
-	end,
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = false,
+		priority = 1000,
+		name = "gruvbox",
+		config = function()
+			vim.o.background = "dark" -- or "light" for light mode
+		end,
+	},
+	{
+		"loctvl842/monokai-pro.nvim",
+		lazy = false,
+		priority = 1001,
+		name = "monokay",
+		config = function()
+			require("monokai-pro").setup({})
+			vim.cmd([[colorscheme monokai-pro]])
+		end,
+	},
 }
