9b00829d3c86cc35bef9f85b1eb4c08dba3b26fb
samuele.villa <samuele.villa.ufficiale@gmail.com>
2025-02-12 18:43:21 +0100

    [NVIM] Enhanced nvim configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,19 @@
+require("core.options") -- Load general options
+require("core.keymaps") -- Load general keymaps
+require("core.snippets") -- Custom code snippetsim.cmd("set expandtab")
+
+-- Setup lazy plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("vim-options")
 require("lazy").setup("plugins")
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,17 +5,49 @@ return {
 		priority = 1000,
 		name = "gruvbox",
 		config = function()
-			vim.o.background = "dark" -- or "light" for light mode
+			-- Default options:
+			local opts = {
+				terminal_colors = true, -- add neovim terminal colors
+				undercurl = true,
+				underline = true,
+				bold = true,
+				italic = {
+					strings = true,
+					emphasis = true,
+					comments = true,
+					operators = false,
+					folds = true,
+				},
+				strikethrough = true,
+				invert_selection = false,
+				invert_signs = false,
+				invert_tabline = false,
+				invert_intend_guides = false,
+				inverse = true, -- invert background for search, diffs, statuslines and errors
+				contrast = "", -- can be "hard", "soft" or empty string
+				palette_overrides = {},
+				overrides = {},
+				dim_inactive = false,
+				transparent_mode = true,
+			}
+
+			local toggle_transparency = function()
+				opts.transparent_mode = not opts.transparent_mode
+				require("gruvbox").setup(opts)
+				vim.cmd("colorscheme gruvbox")
+			end
+
+			require("gruvbox").setup(opts)
+			vim.cmd("colorscheme gruvbox")
+
+			-- Set keybinding to toggle toggle_transparency
+			vim.keymap.set("n", "<leader>bg", toggle_transparency, { noremap = true, silent = true })
 		end,
 	},
 	{
 		"loctvl842/monokai-pro.nvim",
 		lazy = false,
-		priority = 1001,
+		priority = 1000,
 		name = "monokay",
-		config = function()
-			require("monokai-pro").setup({})
-			vim.cmd([[colorscheme monokai-pro]])
-		end,
 	},
 }
